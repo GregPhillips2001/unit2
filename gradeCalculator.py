74eaa4362c7f3205e0f622fba233aeb7a31ef025
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-13 13:50:27 -0400

    Updated from Brython Server: 9/13/2017 1:50:26 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,18 @@
+#Greg Phillips
+#9/13/17
+#gradeCalculator.py - calculates the letter grade to their percentage grade
+
+gradePercent = float(input("Enter your percent grade here: "))
+if(gradePercent<60):
+    print("You earned an incomplete")
+elif(gradePercent>=60 and gradePercent<70):
+    print("You earned a D")
+elif(gradePercent>=70 and gradePercent<80):
+    print("You earned a C")
+elif(gradePercent>=80 and gradePercent<90):
+    print("You earned a B")
+else:
+    print("You earned an A")
+
+
 
