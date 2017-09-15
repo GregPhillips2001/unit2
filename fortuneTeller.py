47809609dff979375d384c35c1608b76920e736e
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-15 14:48:11 -0400

    Updated from Brython Server: 9/15/2017 2:48:09 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,26 @@
+#Greg Phillips
+#9/15/17
+#fortuneTeller.py - tells your fortune
 
+print("Pick red or blue: ")
+color = input(" ")
+
+print("Pick a number from 1-4: ")
+num = float(input(" "))
+
+if(color=="red" and num==1):
+    print("You will die in a car crash")
+elif(color=="red" and num==2):
+    print("You will fail a test")
+elif(color=="red" and num==3):
+    print("You will break your leg")
+elif(color=="red" and num==4):
+    print("You will fall down the stairs")
+elif(color=="blue" and num==1):
+    print("You will be attacked by a pack of wolves")
+elif(color=="blue" and num==2):
+    print("You will fall off a cliff")
+elif(color=="blue" and num==3):
+    print("You will repeat a grade")
+elif(color=="blue" and num==4):
+    print("You will die")
