721b836b39a05420da887a7ef86288b960d595ce
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-15 14:26:24 -0400

    Updated from Brython Server: 9/15/2017 2:26:22 PM

--- warmup3.py
+++ warmup3.py
@@ -1,1 +1,15 @@
+#Greg Phillips
+#9/15/17
+#warmup3.py - says if number is divisible by 3,2
 
+num = float(input("Enter a number here: "))
+
+if(num%2==0 and num%3==0):
+    print(num, "is divisible by both 2 and 3")
+elif(num%2==0):
+    print(num, "is divisible by 2")
+elif(num%3==0):
+    print(num, "is divisible by 3")
+else:
+    print(num, "is not divisble by 2 and 3")
+    
