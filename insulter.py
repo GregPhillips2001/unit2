393f3619a7ad1097dc2c5d3e1a90d0b561571e0b
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-15 14:55:03 -0400

    Updated from Brython Server: 9/15/2017 2:55:02 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,19 @@
+#Greg Phillips
+#9/15/17
+#insulter.py - insults you
+from random import randint
+print("Enter your name: ")
+name = input("")
 
+num = randint(1,5)
+
+if(num==1):
+    print("You are dumb")
+elif(num==2):
+    print("You poor the milk in the bowl before the cerial") 
+elif(num==3):
+    print("You're a caf kid")
+elif(num==4):
+    print("You're a good for nothing waste of space")
+else:
+    print("You failed life")
