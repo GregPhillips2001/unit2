2c8da34ed6645e23d562e3a17727b9481c8db31e
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-18 14:29:38 -0400

    Updated from Brython Server: 9/18/2017 2:29:37 PM

--- coinFlip.py
+++ coinFlip.py
@@ -1,1 +1,12 @@
+#Greg Phillips
+#9/18/17
+#coinFlip.py - randomly prints heads or tails
+from random import randint
+
+num = randint(1,2)
+if num==1:
+    print("Heads")
+else:
+    print("tails")
+
 
