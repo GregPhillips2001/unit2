7fd4e1dbad8059ef17df7fd139821460578e77d4
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-25 14:20:40 -0400

    Updated from Brython Server: 9/25/2017 2:20:39 PM

--- quiz2.py
+++ quiz2.py
@@ -2,5 +2,8 @@
 #9/25/17
 #quiz2.py
 
-num1 = int(input("enter a number"))
-num2 = int(input("enter a number"))
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a 2nd number: "))
+
+if num1==num2:
+    print("The numbers are the same")
