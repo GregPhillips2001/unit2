5dba5321c27dac658a6ba6a58afac74b625865ef
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-09-28 14:35:31 -0400

    Updated from Brython Server: 9/28/2017 2:35:30 PM

--- unitConverter.py
+++ unitConverter.py
@@ -2,22 +2,28 @@
 #9/14/17
 #unitConverter.py - converts units
 
-print("1) Kilometers to miles")
-print("2) Kilograms to Pounds")
-print("3) Liters to Gallons")
-print("4) Celsius to Fahrenheit")
+while True:
+    print("1) Kilometers to miles")
+    print("2) Kilograms to Pounds")
+    print("3) Liters to Gallons")
+    print("4) Celsius to Fahrenheit")
+    print("5) Quit")
 
-num = float(input("Choose a number: "))
+    num = float(input("Choose a number: "))
 
-if(num==1):
-    km = float(input("Enter a number of kilometers: "))
-    print(km, "kilometers is", km*0.621371, "miles")
-elif(num==2):
-    kg = float(input("Enter a number of kilograms: "))
-    print(kg, "kilograms is", kg*2.20462, "pounds")
-elif(num==3):
-    li = float(input("Enter a number of liters: "))
-    print(li, "liters is", li*0.264172, "gallons")
-else:
-    c = float(input("Enter a degrees of Celsius: "))
-    print(c, "degrees Celsius", c*1.8 + 32, "degrees Fahrenheit")
+    if(num==1):
+        km = float(input("Enter a number of kilometers: "))
+        print(km, "kilometers is", km*0.621371, "miles")
+    elif(num==2):
+        kg = float(input("Enter a number of kilograms: "))
+        print(kg, "kilograms is", kg*2.20462, "pounds")
+    elif(num==3):
+        li = float(input("Enter a number of liters: "))
+        print(li, "liters is", li*0.264172, "gallons")
+    elif num==4:
+        c = float(input("Enter a degrees of Celsius: "))
+        print(c, "degrees Celsius", c*1.8 + 32, "degrees Fahrenheit")
+    elif num==5:
+        break
+    else:
+        print("That's not a choice")
